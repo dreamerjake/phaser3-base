9a928c190efefb488eecd5bb8115201ca7ec3077
Jake Silverman <jacobsilverman85@gmail.com>
2018-08-22 08:55:46 -0400

    PORT env variable

--- src/server/index.js
+++ src/server/index.js
@@ -18,4 +18,5 @@ app.get('/test', (req, res) => {
   res.send({test: 'success'})
 })
 
-app.listen(3000, () => console.log('Listening on port 3000'))
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
