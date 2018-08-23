065579d57f7b19bf4fce1ceb6785fe01a7d3a112
Jake Silverman <jacobsilverman85@gmail.com>
2018-08-23 15:30:55 -0400

    mongoose setup, User model, and passport interaction

--- src/server/index.js
+++ src/server/index.js
@@ -1,26 +1,21 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const passport = require('passport')
-const GoogleStrategy = require('passport-google-oauth20').Strategy
+const keys = require('./config/keys')
+
+require('./services/passport')
+require('./models/User')
 
 // Database Setup
-// noinspection JSIgnoredPromiseFromCall
-mongoose.connect('mongodb://mongo:27017/')
-const db = mongoose.connection
-db.on('error', console.error.bind(console, 'connection error:'))
-db.once('open', function () {
-  // successful connection to mongodb
-})
+mongoose.connect(keys.mongoURI)
+// const db = mongoose.connection
+// db.on('error', console.error.bind(console, 'connection error:'))
+// db.once('open', function () {
+//   // successful connection to mongodb
+// })
 
 const app = express()
-
 app.use(express.static('dist'))
-
-app.get('/test', (req, res) => {
-  res.send({test: 'success'})
-})
-
-passport.use(new GoogleStrategy())
+require('./routes/auth')(app)
 
 // const PORT = process.env.PORT || 3000
 const PORT = 3000
